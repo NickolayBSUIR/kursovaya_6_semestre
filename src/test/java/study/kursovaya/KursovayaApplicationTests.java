03359d8302fcf8dcd29b37df8f9ebf74254bf18f
NickolayBSUIR <nickbsuir@gmail.com>
2022-02-17 10:37:11 +0300

    Yes.

---
+++ src/test/java/study/kursovaya/KursovayaApplicationTests.java
@@ -0,0 +1,13 @@
+package study.kursovaya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KursovayaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
